bdc819b7572b32c282b4e39bd1c88d8d1c5f9d94
João Pedro Droval <55903030+PvMDragonic@users.noreply.github.com>
2023-02-06 16:17:42 -0300

    Paralelismo e integração com backend.
    Adicionados 4 processos com 4 threads cada porque estava demorando demais, por se tratar de 130k clãs.

--- nomes_scrapper.py
+++ nomes_scrapper.py
@@ -1,33 +1,45 @@
+from multiprocessing import Process
+from threading import Thread
 from lxml import html
 import fasttext
 import requests
 
+import backend
+
 # Supressa um warning inútil.
 fasttext.FastText.eprint = lambda x: None
+
+# Quantia pra dividir as listas nos processos e threads.
+PARTES = 4
 
 def validar_ptbr(nomes):
     MODEL = fasttext.load_model('lid.176.ftz')
     ptbr = []
 
     for nome in nomes:
-        pagina_clan = f'https://secure.runescape.com/m=clan-home/l=3/a=869/clan/{nome}'
-        conteudo = html.fromstring(requests.get(pagina_clan).content)
-        sobre = " ".join(conteudo.xpath('.//p[@id="aboutOurClan"]/text()'))
-        lema = " ".join(conteudo.xpath('.//p[@id="ourMotto"]/text()'))[1:-2].replace("\n", " ")
+        try:
+            pagina_clan = f'https://secure.runescape.com/m=clan-home/l=3/a=869/clan/{nome}'
+            conteudo = html.fromstring(requests.get(pagina_clan).content)
+            sobre = " ".join(conteudo.xpath('.//p[@id="aboutOurClan"]/text()'))
+            lema = " ".join(conteudo.xpath('.//p[@id="ourMotto"]/text()'))[1:-2].replace("\n", " ")
 
-        # Verifica se é clã PT-BR
-        resultado = [
-            ('__label__pt') in MODEL.predict(sobre, k = 1)[0],
-            ('__label__pt') in MODEL.predict(lema, k = 1)[0]
-        ]
+            # Verifica se é clã PT-BR
+            resultado = [
+                ('__label__pt') in MODEL.predict(sobre, k = 1)[0],
+                ('__label__pt') in MODEL.predict(lema, k = 1)[0]
+            ]
 
-        if any(resultado):
-            ptbr.append(nome)
+            if any(resultado):
+                ptbr.append(nome)
+        except Exception as e:
+            print(f"Erro durante validação de clãs: {e}")
 
-def encontrar_clans(lista: list):
+    backend.adicionar_clans(ptbr)
+
+def encontrar_clans(lista):
     nomes = []
 
-    for num in range(lista):
+    for num in lista:
         pagina = html.fromstring(requests.get(f'https://secure.runescape.com/m=clan-hiscores/l=3/a=869/ranking?ranking=xp_total&table=0&page={num}').content)
         conteudo = pagina.xpath('.//div[@class="tableWrap"]//a')
 
@@ -40,12 +52,26 @@ def encontrar_clans(lista: list):
     
     validar_ptbr(nomes)
 
-def buscar_clans():
+def processo(nomes):
+    nomes = [nomes[i::PARTES] for i in range(PARTES)]
+
+    for i in range(PARTES):
+        Thread(target = encontrar_clans, args = (nomes[i], )).start()
+
+def quantia_clans() -> list:
     pagina = html.fromstring(requests.get('https://secure.runescape.com/m=clan-hiscores/l=3/a=869/ranking').content)
     quantia_paginas = pagina.xpath('.//div[@class="paging"]')
     quantia_paginas = quantia_paginas[0].text_content().split("\n")
+
+    # Encontra o 6413 em ['', '', '', '1', '', '2', '', '3', '', '4', '', '5', '…', '', '6413', '', '→', ''].
     quantia_paginas = int([elem for elem in quantia_paginas if elem.isdigit()][-1])
 
-    encontrar_clans(quantia_paginas)
+    divisao_nomes = [num for num in range(quantia_paginas)]
+    divisao_nomes = [divisao_nomes[i::PARTES] for i in range(PARTES)]
+    return divisao_nomes
 
-buscar_clans()
+def buscar_clans():
+    numero_clans = quantia_clans()
+
+    for i in range(PARTES):
+        Process(target = processo, args = (numero_clans[i], )).start()
