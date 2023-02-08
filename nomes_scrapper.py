30e57f148043b87a4b5b4893698c05ee061c82a3
João Pedro Droval <55903030+PvMDragonic@users.noreply.github.com>
2023-02-08 15:36:47 -0300

    Reduzindo paralelismo pra evitar rate limit.
    Rodar 4 processos com 4 threads cada faz com que o código ultrapasse o
    rate limit de requests da Jagex. Eu tentei abaixar pra 2c/3t, mas alguns
    requests ainda ficavam pra trás, então 2c/2t é o que há.

--- nomes_scrapper.py
+++ nomes_scrapper.py
@@ -3,36 +3,45 @@ from threading import Thread
 from lxml import html
 import fasttext
 import requests
+import time
 
 import backend
 
 # Supressa um warning inútil.
 fasttext.FastText.eprint = lambda x: None
 
-# Quantia pra dividir as listas nos processos e threads.
-PARTES = 4
+# Quantia pra definir o paralelismo dos dados.
+PROCESSOS = 2
+THREADS = 2
 
 def validar_ptbr(nomes):
     MODEL = fasttext.load_model('lid.176.ftz')
     ptbr = []
 
+    def filtrar():
+        pagina_clan = f'https://secure.runescape.com/m=clan-home/l=3/a=869/clan/{nome}'
+        conteudo = html.fromstring(requests.get(pagina_clan).content)
+        sobre = " ".join(conteudo.xpath('.//p[@id="aboutOurClan"]/text()'))
+        lema = " ".join(conteudo.xpath('.//p[@id="ourMotto"]/text()'))[1:-2].replace("\n", " ")
+
+        # Verifica se é clã PT-BR
+        resultado = [
+            ('__label__pt') in MODEL.predict(sobre, k = 1)[0],
+            ('__label__pt') in MODEL.predict(lema, k = 1)[0]
+        ]
+
+        if any(resultado):
+            ptbr.append(nome)
+
     for nome in nomes:
         try:
-            pagina_clan = f'https://secure.runescape.com/m=clan-home/l=3/a=869/clan/{nome}'
-            conteudo = html.fromstring(requests.get(pagina_clan).content)
-            sobre = " ".join(conteudo.xpath('.//p[@id="aboutOurClan"]/text()'))
-            lema = " ".join(conteudo.xpath('.//p[@id="ourMotto"]/text()'))[1:-2].replace("\n", " ")
-
-            # Verifica se é clã PT-BR
-            resultado = [
-                ('__label__pt') in MODEL.predict(sobre, k = 1)[0],
-                ('__label__pt') in MODEL.predict(lema, k = 1)[0]
-            ]
-
-            if any(resultado):
-                ptbr.append(nome)
-        except Exception as e:
-            print(f"Erro durante validação de clãs: {e}")
+            filtrar(nome)
+        except Exception:
+            try:
+                time.sleep(10)
+                filtrar(nome)
+            except Exception as e:
+                print(f"Erro repetido na validação do clã {nome}: {e}")
 
     backend.adicionar_clans(ptbr)
 
@@ -40,25 +49,25 @@ def encontrar_clans(lista):
     nomes = []
 
     for num in lista:
-        pagina = html.fromstring(requests.get(f'https://secure.runescape.com/m=clan-hiscores/l=3/a=869/ranking?ranking=xp_total&table=0&page={num}').content)
-        conteudo = pagina.xpath('.//div[@class="tableWrap"]//a')
+        url = f'https://secure.runescape.com/m=clan-hiscores/l=3/a=869/ranking?ranking=xp_total&table=0&page={num}'
+        pagina = html.fromstring(requests.get(url).content)
+        conteudo = pagina.xpath('.//td[@class="col2"]//a')
 
         for elem in conteudo:
             temp = elem.get("href")
             if 'clanName=' in temp:
                 temp = temp.split("clanName=")[1]
-                if temp not in nomes:
-                    nomes.append(temp)
-    
+                nomes.append(temp)
+
     validar_ptbr(nomes)
 
 def processo(nomes):
-    nomes = [nomes[i::PARTES] for i in range(PARTES)]
+    nomes = [nomes[i::THREADS] for i in range(THREADS)]
 
-    for i in range(PARTES):
+    for i in range(THREADS):
         Thread(target = encontrar_clans, args = (nomes[i], )).start()
 
-def quantia_clans() -> list:
+def buscar_clans():
     pagina = html.fromstring(requests.get('https://secure.runescape.com/m=clan-hiscores/l=3/a=869/ranking').content)
     quantia_paginas = pagina.xpath('.//div[@class="paging"]')
     quantia_paginas = quantia_paginas[0].text_content().split("\n")
@@ -66,12 +75,8 @@ def quantia_clans() -> list:
     # Encontra o 6413 em ['', '', '', '1', '', '2', '', '3', '', '4', '', '5', '…', '', '6413', '', '→', ''].
     quantia_paginas = int([elem for elem in quantia_paginas if elem.isdigit()][-1])
 
-    divisao_nomes = [num for num in range(quantia_paginas)]
-    divisao_nomes = [divisao_nomes[i::PARTES] for i in range(PARTES)]
-    return divisao_nomes
+    quantia_paginas = [num for num in range(quantia_paginas)]
+    quantia_paginas = [quantia_paginas[i::PROCESSOS] for i in range(PROCESSOS)]
 
-def buscar_clans():
-    numero_clans = quantia_clans()
-
-    for i in range(PARTES):
-        Process(target = processo, args = (numero_clans[i], )).start()
+    for i in range(PROCESSOS):
+        Process(target = processo, args = (quantia_paginas[i], )).start()
