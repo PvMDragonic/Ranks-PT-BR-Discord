8da0ed606946199fcd90ac8891b069cebed929f7
João Pedro Droval <55903030+PvMDragonic@users.noreply.github.com>
2023-02-10 11:28:34 -0300

    Correção de falhas.
    O exp_total estava sendo passado na posição errada, e também adicionei
    try-except no caso de algum clan não ser encontrado nos hiscores.

--- exp_scrapper.py
+++ exp_scrapper.py
@@ -9,30 +9,33 @@ def atualizar_exp(clans: list[backend.Clan]):
     estatisticas = []
 
     for clan in clans:
-        pagina_clan = f'https://secure.runescape.com/m=clan-hiscores/l=3/a=869/compare.ws?clanName={clan.nome}'
-        conteudo = html.fromstring(requests.get(pagina_clan).content)
+        try:
+            pagina_clan = f'https://secure.runescape.com/m=clan-hiscores/l=3/a=869/compare.ws?clanName={clan.nome}'
+            conteudo = html.fromstring(requests.get(pagina_clan).content)
 
-        # O primeiro 'OrnamentalBoxContent' é da Visão Geral, enquanto o segundo é das 'Habilidades'.
-        quadro_visao_geral = conteudo.xpath('.//div[@class="OrnamentalBoxContent"]')[0]
+            # O primeiro 'OrnamentalBoxContent' é da Visão Geral, enquanto o segundo é das 'Habilidades'.
+            quadro_visao_geral = conteudo.xpath('.//div[@class="OrnamentalBoxContent"]')[0]
 
-        # Os indices em 'quadro_visao_geral[x]' são referentes a linha do quadro de Visão Geral que é lido.
-        membros = [elem.replace(".", "") for elem in quadro_visao_geral[0].text_content().split("\n") if elem.replace(".", "").isnumeric()][0]
-        nv_total = [elem.replace(".", "") for elem in quadro_visao_geral[1].text_content().split("\n") if elem.replace(".", "").isnumeric()][0]
-        exp_total = [elem.replace(".", "") for elem in quadro_visao_geral[2].text_content().split("\n") if elem.replace(".", "").isnumeric()][0]
-        nv_cb_total = [elem.replace(".", "") for elem in quadro_visao_geral[3].text_content().split("\n") if elem.replace(".", "").isnumeric()][0]
-        nv_fort = [elem.replace(".", "") for elem in quadro_visao_geral[11].text_content().split("\n") if elem.replace(".", "").isnumeric()][0]
+            # Os indices em 'quadro_visao_geral[x]' são referentes a linha do quadro de Visão Geral que é lido.
+            membros = [elem.replace(".", "") for elem in quadro_visao_geral[0].text_content().split("\n") if elem.replace(".", "").isnumeric()][0]
+            nv_total = [elem.replace(".", "") for elem in quadro_visao_geral[1].text_content().split("\n") if elem.replace(".", "").isnumeric()][0]
+            exp_total = [elem.replace(".", "") for elem in quadro_visao_geral[2].text_content().split("\n") if elem.replace(".", "").isnumeric()][0]
+            nv_cb_total = [elem.replace(".", "") for elem in quadro_visao_geral[3].text_content().split("\n") if elem.replace(".", "").isnumeric()][0]
+            nv_fort = [elem.replace(".", "") for elem in quadro_visao_geral[11].text_content().split("\n") if elem.replace(".", "").isnumeric()][0]
 
-        estatisticas.append(
-            backend.Estatisticas(
-                clan.id,
-                datetime.now(),
-                membros,
-                nv_fort,
-                nv_total,
-                exp_total,
-                nv_cb_total
+            estatisticas.append(
+                backend.Estatisticas(
+                    clan.id,
+                    datetime.now(),
+                    membros,
+                    nv_fort,
+                    nv_total,
+                    nv_cb_total,
+                    exp_total
+                )
             )
-        )
+        except IndexError:
+            pass
 
     backend.adicionar_estatisticas(estatisticas)
 
